d6a32cb349a93c797a732988548a1301a6b30f2a
Ekaterina Mozhanova <pandafomka@gmail.com>
2024-10-03 19:53:53 +0300

    register

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import { registerValidation } from "./validations/auth.js";
+import { validationResult } from "express-validator";
+import User from "./models/User.js";
 
 mongoose
   .connect(
-"mongodb+srv://pandafomka:linkinpark081294@cluster0.p3bfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  )
+    "mongodb+srv://pandafomka:linkinpark081294@cluster0.p3bfj.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
 
@@ -14,13 +18,45 @@ const app = express();
 app.use(express.json()); //учим express читать json, без этого
 //запросы от клиента(request) распознаются как undefined
 
-// app.get("/", (req, res) => { 
+// app.get("/", (req, res) => {
 //   res.send("Hello world!");
 // });
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req); //содержит найденные ошибки,
+    //если они есть
+    if (errors.isEmpty()) {
+      const password = req.body.password;
+      // const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, 10);
+      const doc = new User({
+        email: req.body.email,
+        fullname: req.body.fullname,
+        passwordHash: hash,
+        avatarUrl: req.body.avatarUrl,
+      });
+      const user = await doc.save();
+      const token = jwt.sign(
+        {
+          _id: user._id,
+        },
+        "secret123",
+        {
+          expiresIn: "30d", //срок действия токена
+        }
+      );
 
-app.post('/auth/register', registerValidation, (req, res) => {
+      const {passwordHash, ...userData} = user._doc
+      res.json({ ...userData, token });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
+  }
+});
 
-})
 // app.post("/auth/login", (req, res) => {
 //   console.log(req.body); //получим undefined без express.json()
 
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,10 @@
 import { body } from "express-validator";
 
 export const registerValidation = [
-  body("email").isEmail(),
-  body("password").isLength({ min: 5 }),
-  body("fullname").isLength({ min: 3 }),
-  body("avatarUrl").optional().isURL(),
+  body("email", "Неверный формат почты").isEmail(),
+  body("password", "Пароль должен быть минимум 5 символов").isLength({
+    min: 5,
+  }),
+  body("fullname", "Укажите имя").isLength({ min: 3 }),
+  body("avatarUrl", "Неверная ссылка на аватарку").optional().isURL(),
 ];
